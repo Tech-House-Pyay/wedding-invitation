033b9caa40fcc9ccade346e56ecfaafca649f5bf
yethuaungptu <yethuaung481995@gamil.com>
2020-02-11 19:12:41 +0630

    Change name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Wedding Invitation
                 </div>
 
                 <div class="links">
